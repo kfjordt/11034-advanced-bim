6d776ebc8d5d77b9e6db4a6e5b2c10b47797ccb7
Kasper Jordt <kfjordt@gmail.com>
2022-10-13 11:45:52 +0200

    Added comments to the SVG parts of the script

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -11,6 +11,7 @@ from assignment1 import *
 # Comment the SVG parts of the code
 # Clean up CSS
 
+# Various lookup tables
 STOREY_BY_TYPE = {
     "IfcWall": ["PSet_Revit_Constraints", "Base Constraint"],
     "IfcDoor": ["PSet_Revit_Constraints", "Level"],
@@ -34,37 +35,45 @@ UNITS_BY_IFC_UNITS = {
 }
 
 def get_polygons_from_model(model, ifc_type):
+    # Initialize the geometry extraction
     settings = ifcopenshell.geom.settings()
     settings.set(settings.USE_WORLD_COORDS, True)
 
     list_of_polygon_coords = {}
 
+    # Get the appropriate pset path
     pset_path = STOREY_BY_TYPE[ifc_type]
     
     for element in model.by_type(ifc_type):
-
+        # Extract the storey of the given element
         element_psets = ifcopenshell.util.element.get_psets(element)
         element_storey = element_psets[pset_path[0]][pset_path[1]]
 
-
+        # Making sure that the dictionary has the appropriate key
         if element_storey not in list_of_polygon_coords.keys():
             list_of_polygon_coords[element_storey] = []
 
+        # Read the geometry of the element
         try:
             shape = ifcopenshell.geom.create_shape(settings, element)
             faces, verts = shape.geometry.faces, shape.geometry.verts
         except:
             continue
         
+        # Making the flat list be grouped in the vertex pairs
         grouped_verts = [[round(verts[i], 3), round(verts[i + 1], 3)] for i in range(0, len(verts), 3)]
         grouped_faces = [[faces[i], faces[i + 1], faces[i + 2]] for i in range(0, len(faces), 3)]
 
+        # Getting all the polygons associated with a given element
         element_polygons = []
         for face in grouped_faces:
             current_verts = [grouped_verts[idx] for idx in face]
 
             element_polygons.append(Polygon(current_verts))
 
+        # Attempt to merge the different polygons in the element to a single
+        # polygon. Occasionally this will fail due to some elements having
+        # non adjacent polygons, hence the try except statement
         try:
             union_polygon = unary_union(element_polygons)
             x, y = union_polygon.exterior.xy
@@ -78,6 +87,8 @@ def get_polygons_from_model(model, ifc_type):
     return list_of_polygon_coords
 
 def create_parent_html(width, height, size_factor, level):
+    # Each parent entity is the base for the floor plan. Therefore, this is
+    # simply a rectangle with a level attribute 
     html_string = ""
     html_string += f"<div class=\"parent\" level=\"{level}\">\n"
     html_string += f"<svg class=\"svg-one\" width=\"{round(width*size_factor, 2)}\" height=\"{round(height*size_factor, 2)}\">\n"
@@ -92,15 +103,20 @@ def convert_polygons_to_svg(polygon_coords, width, height, x_translation, y_tran
     for ifc_type, polygons in polygon_coords.items():
 
         for element_pts in polygons:
+            # Calculating the translated coordinates of the polygon
             x_coords = [round((pt[0]+x_translation)*size_factor,2) for pt in element_pts]
             y_coords = [round((pt[1]+y_translation)*size_factor,2) for pt in element_pts]
 
+            # Creating a HTML compliant string to hold the polygon coordinates
             element_string = ""
             for x_coord, y_coord in zip(x_coords, y_coords):
                 element_string += f"{x_coord},{y_coord} "
 
+            # Adding color to the shape
             color = COLORS_BY_IFC[ifc_type]
 
+            # Adding the coordinates, color and other properties to the html
+            # element
             html_string += f"<svg class=\"primary\" width=\"{round(width*size_factor, 2)}\" height=\"{round(height*size_factor, 2)}\" fill=\"{color}\">\n"
             html_string += f"<polygon points=\"{element_string[:-1]}\"\>\n"
             html_string += "</svg>\n"
@@ -108,14 +124,23 @@ def convert_polygons_to_svg(polygon_coords, width, height, x_translation, y_tran
     return html_string
 
 def calculate_translation_props(polygons):
+    # The floor plans works by having multiple small polygons be positioned
+    # on top of the parent entity. Therefore, the polygons all have to be 
+    # positioned correctly
+
+    # Exctracting all the point associated with a list of polygons
     all_points = []
     for ifc_type in polygons.values():
         for polygon in ifc_type.values():
             all_points += [item for sublist in polygon for item in sublist]
 
+    # These values determine the bounding box of the floor plan
     min_x, min_y = min([pt[0] for pt in all_points]), min([pt[1] for pt in all_points]) 
     max_x, max_y = max([pt[0] for pt in all_points]), max([pt[1] for pt in all_points]) 
 
+    # The coordinate system has to start at (0,0) since an svg element can't
+    # have negative values in their properties (e.g. a rectangle with a negative
+    # height)
     x_translation, y_translation = 0 - min_x, 0 - min_y
     width, height = max_x - min_x, max_y - min_y
 
@@ -187,6 +212,8 @@ def custom_html(model, volume_unit, materials):
     html_string += "<props2result-  id=\"building_cost\"->\n"
     html_string += "</props2result->\n"
     html_string += "</props2->\n"
+
+    # Add the SVG floor plans beneath the other properties
     html_string += "<floor_plan->\n"
     html_string += create_svg_floor_plans(model)
     html_string += "</floor_plan->\n"
@@ -256,14 +283,21 @@ def material_table_html(unique_materials, volume_unit):
     return html_string
 
 def create_svg_floor_plans(ifc_model):
+    # Read all polygons of the walls, windows and doors of the ifc model
     polygons_by_types = {}
     polygons_by_types["IfcWall"] = get_polygons_from_model(ifc_model, "IfcWall")
     polygons_by_types["IfcWindow"] = get_polygons_from_model(ifc_model, "IfcWindow")
     polygons_by_types["IfcDoor"] = get_polygons_from_model(ifc_model, "IfcDoor")
 
+    # Translate the polygons to comply to the SVG parent entity
     x_translation, y_translation, width, height = calculate_translation_props(polygons_by_types)
+    
+    # When the polygons are read, they will be in the given IFC unit. This variable
+    # is a size factor, i.e. 1 meter = 30 pixels
     SIZE_FACTOR = 30
 
+    # "Transpose" the polygon dictionary to the primary key is the levels and not 
+    # the types
     polygons_by_levels = {}
     for ifc_type, levels in polygons_by_types.items():
         for level, polygons in levels.items():
@@ -272,6 +306,7 @@ def create_svg_floor_plans(ifc_model):
 
             polygons_by_levels[level][ifc_type] = polygons
 
+    # For each level, generate a SVG floor plan
     html_string = ""
     for level in list(polygons_by_types.values())[0].keys():
         html_string += create_parent_html(width, height, SIZE_FACTOR, level)
@@ -282,8 +317,7 @@ def create_svg_floor_plans(ifc_model):
 
 def main():
     # Load IFC model to memory
-    # file_path = input("Enter file path of IFC model here: ")
-    file_path = r'C:\Users\kfjor\Google Drev\DTU\7. semester\11034 Advanced BIM\repo\Assignment 2\Duplex_A_20110907.ifc'
+    file_path = input("Enter file path of IFC model here: ")
     ifc = ifcopenshell.open(file_path)
 
     # Fetch all structural elements in model
