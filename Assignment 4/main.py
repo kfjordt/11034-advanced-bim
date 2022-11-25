cc19bac964973f39b942712682b5263a344f7e14
Kasper Jordt <kfjordt@gmail.com>
2022-11-25 15:40:56 +0100

    simplified the overall structure of the program

--- Assignment 4/main.py
+++ Assignment 4/main.py
@@ -4,21 +4,13 @@ from python_modules.svg_utils import *
 import json
 
 def main():
-    file_path = "models\Duplex_A_20110907.ifc"
-
-    ifc_building = parse_model(file_path)
-    svg_polygons = extract_svg_polygons_from_ifc_building(ifc_building)
-
-    ifc_building_json = serialize_ifc_building_to_json(ifc_building)
-    svg_polygons_json = serialize_svg_polygons_to_json(svg_polygons)
+    ifc_building = parse_model()
     
-    output_json = {
-        "building": ifc_building_json,
-        "polygons": svg_polygons_json
-    }
+    save_polygons_from_building_to_files(ifc_building)
+    ifc_building_json = serialize_ifc_building_to_json(ifc_building)
 
-    with open("scripts/data.json", "w") as f:
-        f.write(json.dumps(output_json))
+    with open("output/data.json", "w") as f:
+        f.write(json.dumps(ifc_building_json))
 
 if __name__ == "__main__":
     main()
